23498efb838900431167da31612162f9ba4a130b
Ciro Cativo <cirocativo@gmail.com>
2022-10-12 20:52:43 -0400

    feat: stories and components created

---
+++ lab-ds/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-extrabold text-2xl text-violet-500">Hi there</h1>
+      <button className="bg-cyan-500 font-bold px-4 py-2 rounded text-white hover:bg-cyan-300">
+        AREBABA
+      </button>
+    </>
+  );
+}
