38f66a5647064c44b9d05739b6bd909d36146c0a
Ciro Cativo <cirocativo@gmail.com>
2022-10-12 22:53:24 -0400

    feat: add publish storybook workflow

--- lab-ds/src/components/Checkbox.tsx
+++ lab-ds/src/components/Checkbox.tsx
@@ -5,7 +5,7 @@ export interface CheckboxProps {}
 
 export function Checkbox({}: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded-">
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500"></Check>
       </CheckboxPrimitive.Indicator>
